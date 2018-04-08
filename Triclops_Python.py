ea4ccb8f9265e326f0b98ea25c0c611f9e3e6ddd
eastonb120 <32558423+eastonb120@users.noreply.github.com>
2018-04-08 21:55:27 +0000

    Update Triclops_Python.py
    This is the updated code from the Sunday meeting. We modified the code for the PIRs.

--- Triclops_Python.py
+++ Triclops_Python.py
@@ -1,73 +1,124 @@
 #This is the file for our python code. I'm sure thats what we'll start with. If not, then I owe everyone a high-five!
 import RPi.GPIO as GPIO
 import time
+
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+
+#INITIAL PIN CONFIGURATION
+#--------------------------------------------------------------------
+#STEP GPIO 22
 Step = 15
-DIR = 13
-Enable = 36
-Rotate_1Switch = 40
-Rotate_2Switch = 38
-Rotate_3KillSwitch = 32
 GPIO.setup(Step, GPIO.OUT)
+#--------------------------------------------------------------------
+#DIR GPIO 27
+DIR = 13
 GPIO.setup(DIR, GPIO.OUT)
+#--------------------------------------------------------------------
+#ENABLE GPIO 16
+Enable = 36
+#--------------------------------------------------------------------
+#clockWise_PIR GPIO 21
+clockWise_PIR = 40
+#--Push Button Simulation
+#GPIO.setup(clockWise_PIR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-##Switch Setup
-GPIO.setup(Rotate_1Switch, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(Rotate_2Switch, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(Rotate_3KillSwitch, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#--Actual PIR
+GPIO.setup(clockWise_PIR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#--------------------------------------------------------------------
+#counterClockWise_PIR GPIO 20
+counterClockWise_PIR = 38
+#--Push Button Simulation
+#GPIO.setup(counterClockWise_PIR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-##Move one direction
+#--Actual PIR
+GPIO.setup(counterClockWise_PIR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#--------------------------------------------------------------------
+#center_PIR GPIO 12
+center_PIR = 32
+
+#--Push Button Simulation
+#GPIO.setup(center_PIR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+#--Actual PIR
+GPIO.setup(center_PIR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#--------------------------------------------------------------------
+
+#Define boolean for rotation behavior. Linked to DIR GPIO
+clockwise = False
+counterClockwise = True
+
+
+#Rotate Motor Clockwise
 GPIO.output(DIR, False)
 for i in range(800):
     GPIO.output(Step, True)
     GPIO.output(Step, False)
     time.sleep(0.001)
     
-##Move one direction
+#Rotate Motor Counter Clockwise
 GPIO.output(DIR, True)
 for i in range(800):
     GPIO.output(Step, True)
     GPIO.output(Step, False)
     time.sleep(0.001)
 
-DIRBool = True
-clockwise = True
-counterClockwise = False
-whileFlag = True
-while True:
-    input_state = GPIO.input(Rotate_1Switch)
-    #print(input_state)
-    clockWiseFlag = True
-    if (input_state == False):
-        while (clockWiseFlag):
-            #print("button pressed")
-            GPIO.output(DIR, counterClockwise)
-            GPIO.output(Step, True)
-            GPIO.output(Step, False)
-            time.sleep(0.001)
-            input_state = GPIO.input(Rotate_1Switch)
-            input_state3 = GPIO.input(Rotate_3KillSwitch)
-            if (input_state3 == False):
-                clockWiseFlag = False
-    input_state2 = GPIO.input(Rotate_2Switch)
-    #print(input_state2)
-    counterClockWiseFlag = True
-    if (input_state2 == False):
-        while (counterClockWiseFlag):
-            #DIRBool = not(DIRBool)
-            GPIO.output(DIR, clockwise)
-            GPIO.output(Step, True)
-            GPIO.output(Step, False)
-            time.sleep(0.001)
-            input_state = GPIO.input(Rotate_1Switch)
-            input_state3 = GPIO.input(Rotate_3KillSwitch)
-            if (input_state3 == False):
-                counterClockWiseFlag = False
 
-##    input_state3 = GPIO.input(Rotate_3KillSwitch)
-##    if (input_state3 == False):
-##        whileFlag = False
-##        
+
+#Infinite Main Loop (Allows logic to operate until program terminated)
+while True:  
+
+    print("Loop Begin")
+    #Clockwise Rotation
+    #Swap from False to TRUE
+    if(GPIO.input(clockWise_PIR) == True):
+        
+        print("\nClockwise PIR Triggered")
+        #Set Rotation Direction to Clockwise
+        GPIO.output(DIR, clockwise)
+        
+        while(True):
+            
+            #Step in Clockwise Direction
+            GPIO.output(Step, True)
+            GPIO.output(Step, False)
+            time.sleep(0.1)
+
+            #If Center PIR is Triggered, stop stepping and exit to main loop
+            if(GPIO.input(center_PIR) == True):
+                print("Center PIR Triggered")
+                time.sleep(3)
+                break
+
+
+   
+
+    #Counter Clockwise Rotation
+    if(GPIO.input(counterClockWise_PIR) == True):
+        
+        print("\nCounter Clockwise PIR Triggered")
+        #Set Rotation Direction to Counter Clockwise
+        GPIO.output(DIR, counterClockwise)
+        
+        while (True):
+            
+            #Step in Counter Clockwise Direction
+            GPIO.output(Step, True)
+            GPIO.output(Step, False)
+            time.sleep(0.1)
+
+            #If Center PIR is Triggered, stop stepping and exit to main loop
+            if (GPIO.input(center_PIR) == True):
+                print("Center PIR Triggered")
+                time.sleep(3)
+                break
+            
+    print("Sleeping")
+
+    print("Wake up!")
+        
+        
+
         
         
